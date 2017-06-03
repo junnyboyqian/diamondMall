ea21f9e317adc905861c952f8e89555fbfcc2f6f
李小刚 <xg.l@dface.cn>
2017-06-03 16:10:50 +0800

    update

--- src/service/cart/service.js
+++ src/service/cart/service.js
@@ -4,11 +4,12 @@ module.service('cartFlowData', function ($http) {
     let self = this;
     //  管理组列表
     self.cartList = {};
-    self.getCartList = function () {
+    self.getCartList = function (formData) {
         var url = '/api/cartList';
         return $http({
             method: 'GET',
             url: url,
+            params: formData,
             headers: {
               'Content-Type': 'application/x-www-form-urlencoded'
             }
