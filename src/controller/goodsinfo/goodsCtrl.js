6ad74d4a171b41ad7262d0d1c951c04c5e4c90ff
李小刚 <xg.l@dface.cn>
2017-06-03 15:56:58 +0800

    update

--- src/controller/goodsinfo/goodsCtrl.js
+++ src/controller/goodsinfo/goodsCtrl.js
@@ -55,6 +55,10 @@ module.controller('goodsitemCtrl', function ($scope, $stateParams, goodsitemData
         $scope.formData.specId = $scope.goodsInfo.defaultSpec
         $scope.formData.isLz = 0
         $scope.formData.quantity = 1
+        if(sessionStorage["userInfo"]){
+            alert(1)
+            $scope.formData.accessToken = JSON.parse(sessionStorage["userInfo"]).accessToken
+        }
         if ($scope.formData.ringSize === '0') {
             alert("请选择手指尺寸")
             return
